1332a7547bacb734de9a31ced3b4f5e5295aba76
Bertil <ipsen.bertil@gmail.com>
2025-05-27 12:18:51 +0200

    Remove underscore from downlaod filename

--- scripts/app.js
+++ scripts/app.js
@@ -117,7 +117,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     function downloadImage() {
         const link = document.createElement('a');
-        link.download =  `${'processed_' + Date.now()}.jpg`;
+        link.download =  `${'processed' + Date.now()}.jpg`;
         link.href = canvas.toDataURL('image/jpeg', 0.9);
         link.click();
     }  
